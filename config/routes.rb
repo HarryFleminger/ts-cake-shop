596bf28c7d0d0a16bdfb0a67fcd587db61bcf95e
harry fleminger <harry.fleminger@hotmail.com>
2022-12-19 18:21:09 +0000

    Added the main routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :users
+  resources :products
+  resources :orders do
+    resources :line_items, except: [:index]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
