800f62b007b7d072ee2861ad3616b0091f2e1064
harry fleminger <harry.fleminger@hotmail.com>
2022-12-19 16:40:46 +0000

    Added the associations between user's, line_items through orders, and products through line_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+  has_many :line_items
+  has_many :orders, through: :line_items
 end
