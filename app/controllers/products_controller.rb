90f60ceccf5f1559aaa835786db6fff7e9da08d5
harry fleminger <harry.fleminger@hotmail.com>
2022-12-20 11:01:02 +0000

    Added index and show controllers for products. Added basic view for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def index
-    @prodcuts = Product.all
+    @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
 end
