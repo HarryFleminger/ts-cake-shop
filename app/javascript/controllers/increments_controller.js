2ad8a3ca6aeb7eb00019bcc62fa2e91e0c0d6ade
Harry Fleminger <harry.fleminger@hotmail.com>
2023-02-06 20:05:25 +0000

    Added the stimulus controller and basic logic for incrementing quantity. Still need to style it properly and decide how to show the value going up and down

--- app/javascript/controllers/increments_controller.js
+++ app/javascript/controllers/increments_controller.js
@@ -2,17 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="increments"
 export default class extends Controller {
-  static targets = ["quantity"];
+  static targets = [ "quantity" ]
   connect() {
-    console.log("connected!")
-    console.log(quantity)
+    console.log("hello world")
+  }
+  increment(event) {
+    event.preventDefault()
+    this.quantityTarget.value = parseInt(this.quantityTarget.value) + 1
   }
 
-  increment() {
-    this.quantityTarget.value = parseInt(this.quantityTarget.value) + 1;
-  }
-
-  decrement() {
-    this.quantityTarget.value = parseInt(this.quantityTarget.value) - 1;
+  decrement(event) {
+    event.preventDefault()
+    this.quantityTarget.value = parseInt(this.quantityTarget.value) - 1
   }
 }
