350548a34db89d44baa6b98cf583a898dc05ef32
Daniel Pearson <pearsondg@gmail.com>
2023-02-10 16:44:17 +0000

    basket button price fixed and navbar button accessible to guest users

--- app/javascript/controllers/increments_controller.js
+++ app/javascript/controllers/increments_controller.js
@@ -5,23 +5,23 @@ export default class extends Controller {
   static targets = [ "quantity", "show", "price", "basket" ]
 
   connect() {
-    this.basketTarget.value = `Add to basket £` + `${parseInt(this.priceTarget.innerHTML) * this.quantityTarget.value}`
+    this.basketTarget.value = `Add to basket £` + `${(parseFloat(this.priceTarget.innerHTML) * this.quantityTarget.value).toFixed(2)}`
   }
   increment(event) {
     event.preventDefault()
     if (this.quantityTarget.value < 10) {
-      this.quantityTarget.value = parseInt(this.quantityTarget.value) + 1
-      this.showTarget.innerHTML = parseInt(this.showTarget.innerHTML) + 1
-      this.basketTarget.value = `Add to basket £` + `${parseInt(this.priceTarget.innerHTML) * this.quantityTarget.value}`
+      this.quantityTarget.value = parseFloat(this.quantityTarget.value) + 1
+      this.showTarget.innerHTML = parseFloat(this.showTarget.innerHTML) + 1
+      this.basketTarget.value = `Add to basket £` + `${(parseFloat(this.priceTarget.innerHTML) * this.quantityTarget.value).toFixed(2)}`
     }
   }
 
   decrement(event) {
     event.preventDefault()
     if (this.quantityTarget.value > 1) {
-      this.quantityTarget.value = parseInt(this.quantityTarget.value) - 1
-      this.showTarget.innerHTML = parseInt(this.showTarget.innerHTML) - 1
-      this.basketTarget.value = `Add to basket £` + `${parseInt(this.priceTarget.innerHTML) * this.quantityTarget.value}`
+      this.quantityTarget.value = parseFloat(this.quantityTarget.value) - 1
+      this.showTarget.innerHTML = parseFloat(this.showTarget.innerHTML) - 1
+      this.basketTarget.value = `Add to basket £` + `${(parseFloat(this.priceTarget.innerHTML) * this.quantityTarget.value).toFixed(2)}`
     }
   }
 }
