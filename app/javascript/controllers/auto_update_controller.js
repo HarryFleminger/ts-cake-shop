2d763f8eaea995134bfd7273ec3de43d70863409
Harry Fleminger <harry.fleminger@hotmail.com>
2023-02-13 20:15:37 +0000

    Added increment/decrement buttons to product cards so they can be changed within the basket

--- app/javascript/controllers/auto_update_controller.js
+++ app/javascript/controllers/auto_update_controller.js
@@ -2,15 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="auto-update"
 export default class extends Controller {
-  static targets = ["revealButton", "quantity"]
+  static targets = ["quantity", "show"]
 
 
   connect() {
-    this.quantityTarget.addEventListener("change", this.submit.bind(this));
+    this.quantityTarget.addEventListener("input", this.submit.bind(this));
   }
 
   submit(event) {
     event.preventDefault();
     this.element.submit();
+  }
+
+  increment(event) {
+    event.preventDefault()
+    if (this.quantityTarget.value < 10) {
+      this.quantityTarget.value = parseInt(this.quantityTarget.value) + 1
+      this.element.submit();
+    }
+  }
+
+  decrement(event) {
+    event.preventDefault()
+    if (this.quantityTarget.value > 1) {
+      this.quantityTarget.value = parseInt(this.quantityTarget.value) - 1
+      this.element.submit();
+    }
   }
 }
