43b42e2d8f65bec7c01ed4c994e0571155ae2864
Harry Fleminger <harry.fleminger@hotmail.com>
2023-03-13 17:39:03 +0000

    Added partial for products and changed seed to add relevant images from website

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,36 @@
 puts "Generating cakes!"
-cheese_cake = Product.new(name: "Cheese Cake", price: 15, description: "Cheesy goodness")
-sponge_cake = Product.new(name: "Sponge Cake", price: 10, description: "Spongey")
-birthday_cake = Product.new(name: "Birthday Cake", price: 5, description: "Creamy victorianus")
+kripsy_kreme = Product.new(name: "Kripsy Kreme Cake", price: 50, description: "Cheesy goodness")
+hulk_cake = Product.new(name: "Hulk Cake", price: 35, description: "A marvel universe cake")
+jungle_cake = Product.new(name: "The Jungle Cake", price: 40, description: "A wild cake")
+minimal_cake = Product.new(name: "The Minimal Cake", price: 38, description: "Minimal but tasty")
+princess_cake = Product.new(name: "A Princess Cake", price: 66, description: "A princesses dream")
+pokemon_cake = Product.new(name: "The Pokey Cake", price: 38, description: "A pokemon lovers cake")
 
 puts "Attaching photos"
-cheese_cake_photo = URI.open("https://res.cloudinary.com/dblvfwtds/image/upload/v1675593590/photo-1673280115847-ca8db581acbb_plg3uc.jpg")
-cheese_cake.photos.attach(io: cheese_cake_photo, filename: "Cheese_cake.jpg", content_type: "image/jpg")
+kripsy_kreme_photo = URI.open("https://res.cloudinary.com/dblvfwtds/image/upload/v1678728066/289481912_341253488161197_2848639002910872896_n_mr4op1.jpg")
+kripsy_kreme.photos.attach(io: kripsy_kreme_photo, filename: "kripsy_kreme.jpg", content_type: "image/jpg")
 
-sponge_cake_photo = URI.open("https://res.cloudinary.com/dblvfwtds/image/upload/v1675593488/photo-1675227977042-a572dac762be_o7ednr.jpg")
-sponge_cake.photos.attach(io: sponge_cake_photo, filename: "Sponge_cake.jpg", content_type: "image/jpg")
+hulk_cake_photo = URI.open("https://res.cloudinary.com/dblvfwtds/image/upload/v1678728066/288913300_554856272796740_5803513019029902746_n_vvgjj6.jpg")
+hulk_cake.photos.attach(io: hulk_cake_photo, filename: "hulk_cake.jpg", content_type: "image/jpg")
 
-birthday_cake_photo = URI.open("https://res.cloudinary.com/dblvfwtds/image/upload/v1675593531/photo-1673417284575-cb29c8631125_gmcwbo.jpg")
-birthday_cake.photos.attach(io: birthday_cake_photo, filename: "Birthday_cake.jpg", content_type: "image/jpg")
+jungle_cake_photo = URI.open("https://res.cloudinary.com/dblvfwtds/image/upload/v1678728066/291921980_421220526549742_2096783482414313376_n_njxilc.jpg")
+jungle_cake.photos.attach(io: jungle_cake_photo, filename: "jungle_cake.jpg", content_type: "image/jpg")
 
-cheese_cake.save!
-sponge_cake.save!
-birthday_cake.save!
+minimal_cake_photo = URI.open("https://res.cloudinary.com/dblvfwtds/image/upload/v1678728066/289481912_341253488161197_2848639002910872896_n_mr4op1.jpg")
+minimal_cake.photos.attach(io: minimal_cake_photo, filename: "minimal_cake.jpg", content_type: "image/jpg")
+
+princess_cake_photo = URI.open("https://res.cloudinary.com/dblvfwtds/image/upload/v1678728066/312278684_509697074352981_98360624366357888_n_vdcube.jpg")
+princess_cake.photos.attach(io: princess_cake_photo, filename: "princess_cake.jpg", content_type: "image/jpg")
+
+pokemon_cake_photo = URI.open("https://res.cloudinary.com/dblvfwtds/image/upload/v1678728065/312326843_364092999210077_7774964861317527094_n_kb69sf.jpg")
+pokemon_cake.photos.attach(io: pokemon_cake_photo, filename: "pokemon_cake.jpg", content_type: "image/jpg")
+
+kripsy_kreme.save!
+hulk_cake.save!
+jungle_cake.save!
+minimal_cake.save!
+princess_cake.save!
+pokemon_cake.save!
 
 puts "Generating main user"
 main_user = User.create!(first_name: "Ben", last_name: "Franky", email: "harry.fleminger@hotmail.com", password: 123456)
