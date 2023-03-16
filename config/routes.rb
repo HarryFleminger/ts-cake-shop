be7589129436a7440627ce74936d183ccd586ae4
Harry Fleminger <harry.fleminger@hotmail.com>
2023-03-16 15:52:18 +0000

    Updated home page route and change the header to account for the new route for updating the correct css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "custom_cakes#index"
 
   resources :users do
     resources :orders, only: [:show, :index]
