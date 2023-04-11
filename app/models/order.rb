d0b0fa5ceedc205bb16190de9027ee6ba94a2866
Harry Fleminger <harry.fleminger@hotmail.com>
2023-04-11 15:26:48 +0100

    Heroku bug fixes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
   accepts_nested_attributes_for :line_items
 end
